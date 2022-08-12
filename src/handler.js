9af308fb88ad1490c5f466d5eb74fa113382fcfa
fitrafepUGM <fitriansyahekaputra2001@mail.ugm.ac.id>
2022-08-12 13:27:39 +0700

    add additional structure of book

--- src/handler.js
+++ src/handler.js
@@ -1,5 +1,16 @@
+const nanoid = require('nanoid');
+
 const addBookHandler = (request, h) => {
+    //requests
     const { name, year, author, summary, publisher, pageCount, readPage, reading} = request.payload;
+
+    //additional strucutre
+    const id = nanoid(16);
+    const finished = () => {
+        return readPage === pageCount;
+    }
+    const insertedAt = new Date().toISOString();
+    const updatedAt = insertedAt;
 }
 
 module.exports = { addBookHandler };
