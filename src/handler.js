fa5a827ea0210b78b3e2d86b966ab0b1d4adc245
fitrafepUGM <fitriansyahekaputra2001@mail.ugm.ac.id>
2022-08-12 17:38:23 +0700

    add error responses

--- src/handler.js
+++ src/handler.js
@@ -1,10 +1,32 @@
 const nanoid = require('nanoid');
+const books = require('./books');
 
 const addBookHandler = (request, h) => {
     //requests
     const { name, year, author, summary, publisher, pageCount, readPage, reading} = request.payload;
 
     //additional strucutre
+    //error responses
+    //if name is empty
+    if (bame === undefined){
+        const response = h.response({
+            status: 'fail',
+            message: 'Gagal menambahkan buku. Mohon isi nama buku',
+        });
+        response.code(400);
+        return response;
+    }
+
+    //if readPage > pageCount
+    if (readPage > pageCount){
+        const response = h.response({
+            status: 'fail',
+            message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
+        });
+        response.code(400);
+        return response;
+    }
+    
     const id = nanoid(16);
     const finished = () => {
         return readPage === pageCount;
@@ -16,7 +38,7 @@ const addBookHandler = (request, h) => {
     const newBook = {
         id, name, year, author, summary, publisher, pageCount, readPage, finished, reading, insertedAt, updatedAt
     };
-    
+
     //push newBook to books
     books.push(newBook);
 }
