a87bc39677e54dd152b7e676c5cd143d0b68903c
fitrafepUGM <fitriansyahekaputra2001@mail.ugm.ac.id>
2022-08-12 18:43:12 +0700

    add getAllBooksHandler 5/6 pass

--- src/handler.js
+++ src/handler.js
@@ -64,4 +64,30 @@ const addBookHandler = (request, h) => {
     return response;
 }
 
-module.exports = { addBookHandler };
+const getAllBooksHandler = (request, h) => {
+    const { name, reading, finished } = request.query;
+
+    let filteredBooks = books;
+    if(name!== undefined){
+        filteredBooks = filteredBooks.filter((book) => book.name.toLowerCase().includes(name.toLowerCase()));
+    }else if(reading !== undefined){
+        filteredBooks = filteredBooks.filter((book) => book.reading ===!!Number(reading));
+    }else if(finished !== undefined){
+        filteredBooks = filteredBooks.filter((book) => book.finished ===!!Number(finished));
+    }
+    const response = h.response({
+        status: 'success',
+        data: {
+            books: filteredBooks.map((book) => ({
+                id: book.id,
+                name: book.name,
+                year: book.year,
+            })),
+        },
+    });
+
+    response.code(200);
+    return response;
+};
+
+module.exports = { addBookHandler, getAllBooksHandler };
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,20 @@
-const { addBookHandler } = require("./handler");
+const { addBookHandler, getAllBooksHandler } = require("./handler");
 
 const routes = [
     {
         method: 'POST',
         path: '/books',
         handler: addBookHandler,
+        options: {
+            cors: {
+                origin: ['*'],
+            }
+        }
+    },
+    {
+        method: 'GET',
+        path: '/books',
+        handler: getAllBooksHandler,
         options: {
             cors: {
                 origin: ['*'],
