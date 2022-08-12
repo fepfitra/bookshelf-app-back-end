7888d363991929a7726be928a0cea8cbd8b578c5
fitrafepUGM <fitriansyahekaputra2001@mail.ugm.ac.id>
2022-08-12 13:05:22 +0700

    fix bug to running server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,20 @@
-console.log("test");
+const Hapi = require('@hapi/hapi');
+// const route = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  // server.route(route);
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
